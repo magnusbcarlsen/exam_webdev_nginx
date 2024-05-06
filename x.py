f18ed28fa87248a49277acc902b95f800a231d67
Snoopsie1 <oliverrasoli@gmail.com>
2024-05-06 15:50:20 +0200

    validation and cookie stuff

--- x.py
+++ x.py
@@ -13,7 +13,7 @@ def dict_factory(cursor, row):
 ##############################
 
 def db():
-    db = sqlite3.connect(os.getcwd()+"/company.db")  
+    db = sqlite3.connect(os.getcwd()+"/database/company.db")  
     db.row_factory = dict_factory
     return db
 
@@ -21,14 +21,39 @@ def db():
 COOKIE_SECRET_KEY = "429c6db7-0c7f-4836-9dbb-315ba228b8a9#f328df55-6862-4341-8611-e97e6585b9ab"
 ##############################
 
+def validate_user_logged():
+    user = request.get_cookie("user", secret=COOKIE_SECRET)
+    if user is None: raise Exception("user must login", 400)
+    return user
+
+##############################
+
+def is_user_logged_in():
+    user = request.get_cookie("user", secret=COOKIE_SECRET)
+    if user:
+        return True
+    else:
+        return False
+
+##############################
+
 def validate_logged():
     # Prevent logged pages from caching
     response.add_header("Cache-Control", "no-cache, no-store, must-revalidate")
     response.add_header("Pragma", "no-cache")
     response.add_header("Expires", "0")  
-    user_id = request.get_cookie("id", secret = COOKIE_SECRET_KEY)
-    if not user_id: raise Exception("***** user not logged *****", 400)
-    return user_id
+    user = request.get_cookie("user", secret = COOKIE_SECRET_KEY)
+    if not user: raise Exception("***** user not logged *****", 400)
+    return user
+
+
+##############################
+
+def is_cookie_https():
+    if 'PYTHONANYWHERE_DOMAIN' in os.environ:
+        return True;
+    else:
+        return False;
 
 
 ##############################
@@ -48,10 +73,10 @@ def validate_user_id():
 USER_EMAIL_MAX = 100
 USER_EMAIL_REGEX = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
 
-def validate_email():
+def validate_user_email():
     error = f"email invalid"
     user_email = request.forms.get("user_email", "").strip()
-    if not re.match(EMAIL_REGEX, user_email): raise Exception(error, 400)
+    if not re.match(USER_EMAIL_REGEX, user_email): raise Exception(error, 400)
     return user_email
 
 ##############################
@@ -95,7 +120,7 @@ USER_PASSWORD_MIN = 6
 USER_PASSWORD_MAX = 50
 USER_PASSWORD_REGEX = "^.{6,50}$"
 
-def validate_password():
+def validate_user_password():
     error = f"password {USER_PASSWORD_MIN} to {USER_PASSWORD_MAX} characters"
     user_password = request.forms.get("user_password", "").strip()
     if not re.match(USER_PASSWORD_REGEX, user_password): raise Exception(error, 400)
@@ -109,23 +134,3 @@ def confirm_password():
   user_confirm_password = request.forms.get("user_confirm_password", "").strip()
   if user_password != user_confirm_password: raise Exception(error, 400)
   return user_confirm_password
-
-##############################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

9c89671cba40626901f928346efcafd70c0a1372
magnusbcarlsen <magn4496@stud.kea.dk>
2024-05-07 13:09:42 +0200

    cols fucker og block button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import default_app, error, get, post, redirect, response,request, run, static_file, template
+from bottle import default_app, error, get, post, redirect, response, request, run, static_file, template
 import sqlite3
 from icecream import ic
 import bcrypt
@@ -46,8 +46,13 @@ def _():
         except:
             pass
         ic(properties)
-        print(properties)
-        return template('index.html', properties=properties)
+        is_admin = False
+        try:
+            is_admin = x.get_cookie_data()['user_role_fk'] == '2'
+        except:
+            pass
+        # print(properties)
+        return template('index.html', properties=properties, is_logged=is_logged, is_admin=is_admin)
     except Exception as ex:
         ic(ex)
         return "No no noo, more lemon pledge"
@@ -65,9 +70,9 @@ def _():
     return template("signup.html")
 ##############################
 # Serve 404 Not Found
-@error(404)
-def _(error):
-    return template('error.html', is_logged=x.is_user_logged_in())
+# @error(404)
+# def _(error):
+#     return template('error.html', is_logged=x.is_user_logged_in())
 ############################## admin
 import routes.login
 import routes.logout
@@ -77,6 +82,8 @@ import routes.signup
 import routes.verify
 ##############################
 import routes.get_more_properties
+##############################
+import routes.admin_block_property
 
 ##############################
 @post('/a0eb0d133292439b941c063361315db6')
@@ -88,6 +95,7 @@ def git_update():
   return ""
 
 ##############################
+
 
 if 'PYTHONANYWHERE_DOMAIN' in os.environ:
     application = default_app()

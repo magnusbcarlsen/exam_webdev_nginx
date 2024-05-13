06eeab1b9204d200cb3c2915982937f5f409cf9f
Snoopsie1 <oliverrasoli@gmail.com>
2024-05-13 09:52:10 +0200

    users can see their own properties

--- routes/profile.py
+++ routes/profile.py
@@ -1,15 +1,26 @@
-from bottle import get, template
+from bottle import get, template, redirect, HTTPResponse
 from icecream import ic
 import x
 
 @get('/profile')
 def _():
     try:
-        user_pk = x.get_cookie_data()['user_pk']
+        user_data = x.get_cookie_data()
+        
+        if user_data is None:
+            redirect('/login')
+
         db = x.db()
-        q = db.execute('SELECT * FROM users WHERE user_pk = ?', (user_pk,))
-        user = q.fetchone()
-        return template('profile.html', user=user)
+
+        user_q = db.execute('SELECT * FROM users WHERE user_pk = ?', (user_data['user_pk'],))
+        user = user_q.fetchone()
+    
+        property_q = db.execute('SELECT * FROM properties WHERE property_user_fk = ?', (user_data['user_pk'],))
+        users_properties = property_q.fetchall()
+
+        return template('profile.html', user=user, users_properties=users_properties, is_logged=True, is_admin=False)
+    except HTTPResponse:
+        raise
     except Exception as ex:
         ic(ex)
     finally:

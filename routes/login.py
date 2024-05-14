2a5e6dc68a6376a8a8d41ad313f2098fa2610c60
Snoopsie1 <oliverrasoli@gmail.com>
2024-05-14 11:02:59 +0200

    added pw unhashing

--- routes/login.py
+++ routes/login.py
@@ -1,5 +1,6 @@
 from bottle import post, response, request, template
 from icecream import ic
+import bcrypt
 import json
 import x
 
@@ -7,23 +8,24 @@ import x
 @post("/login")
 def _():
   try:
-    # TODO: Add logic for if they try and log on to a user who's user_deleted_at != 0
     user_email = x.validate_user_email()
     user_password = x.validate_user_password()
     db = x.db()
-    q = db.execute('SELECT * FROM users WHERE user_email = ? AND user_password = ?', (user_email, user_password))
+    q = db.execute('SELECT * FROM users WHERE user_email = ?', (user_email,))
     user = q.fetchone()
-    ic(user['user_is_verified'])
+    ic(user)
     if user:
+        if not bcrypt.checkpw(user_password.encode(), user["user_password"]): raise Exception("Invalid credentials", 400)
         if '1' in user['user_is_verified']:
             if user['user_deleted_at'] != '0':
-                # return template("profile_restore", user_pk=user['user_pk'])
-                ic(user['user_pk'])
                 return f"""
                     <template mix-redirect="/profile_restore_agent/{user['user_pk']}">
                     </template>
                 """
             else:
+                for key in user:
+                    if isinstance(user[key], bytes):
+                        user[key] = user[key].decode('utf-8')
                 user_data = json.dumps(user)
                 response.set_cookie("user", user_data, secret=x.COOKIE_SECRET, httponly=True, secure=x.is_cookie_https())
                 return """

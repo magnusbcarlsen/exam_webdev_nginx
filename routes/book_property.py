7f37885adbdcbe9eea6169bbb7cc1d9072f3cae0
Snoopsie1 <oliverrasoli@gmail.com>
2024-05-31 14:57:17 +0200

    drenge

--- routes/book_property.py
+++ routes/book_property.py
@@ -6,25 +6,31 @@ from icecream import ic
 @post ("/book_property/<property_pk>")
 def _(property_pk):
     try:
-        db = x.db()
-        user_pk = x.get_cookie_data()['user_pk']
-        random_id = uuid.uuid4().hex
-        booking_pk = random_id
-        q = db.execute("INSERT INTO bookings (booking_pk, booking_user_fk, booking_property_fk) VALUES (?, ?, ?)", (booking_pk, user_pk, property_pk))
-        db.commit()
-        return f"""
-            <template mix-target="[id='{property_pk}']" mix-replace>
-                <form id="{property_pk}">
-                    <button class="bg-accentCol border border-transparent rounded-lg text-white hover:border-accentCol hover:text-accentCol hover:bg-white duration-100 w-full py-2"
-                        mix-data="[id='{property_pk}']"
-                        mix-put="/unbook_property/{property_pk}"
-                    >
-                        unbook property
-                    </button>
-                </form>
-            </template>
-        """
-
+        is_user_logged_in = x.is_user_logged_in()
+        if (is_user_logged_in):
+            db = x.db()
+            user_pk = x.get_cookie_data()['user_pk']
+            random_id = uuid.uuid4().hex
+            booking_pk = random_id
+            q = db.execute("INSERT INTO bookings (booking_pk, booking_user_fk, booking_property_fk) VALUES (?, ?, ?)", (booking_pk, user_pk, property_pk))
+            db.commit()
+            return f"""
+                <template mix-target="[id='{property_pk}']" mix-replace>
+                    <form id="{property_pk}">
+                        <button class="bg-accentCol border border-transparent rounded-lg text-white hover:border-accentCol hover:text-accentCol hover:bg-white duration-100 w-full py-2"
+                            mix-data="[id='{property_pk}']"
+                            mix-put="/unbook_property/{property_pk}"
+                        >
+                            unbook property
+                        </button>
+                    </form>
+                </template>
+            """
+        else:
+            return """
+                <template mix-redirect="/login">
+                </template>
+            """
     except Exception as ex:
         ic(ex)
         return ex
@@ -52,7 +58,6 @@ def _(property_pk):
                 </form>
             </template>
         """
-
     except Exception as ex:
         ic(ex)
         return ex

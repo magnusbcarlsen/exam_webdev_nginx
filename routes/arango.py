8c3e307ffb232d4c269851b519024bbc62deca2b
Snoopsie1 <oliverrasoli@gmail.com>
2024-06-03 10:16:34 +0200

    arango shizzle

--- routes/arango.py
+++ routes/arango.py
@@ -3,11 +3,11 @@ import requests
 from icecream import ic
 import x
 
-url = 'http://arangodb:8529/_api/cursor'
+url = 'http://arangodb:8529/_api/'
 
 def execute_query(query):
     body = { "query" : query }
-    response = requests.post(url, json = body)
+    response = requests.post(url+"cursor", json = body)
 
     if response.status_code == 201:
         data = response.json()
@@ -16,90 +16,100 @@ def execute_query(query):
         raise Exception(f'Request failed with status code: {response.status_code}')
     
     return result
-############################################################
-def create_item(item_name, item_description):
+
+# CREATE ####################################################
+def create_item(collection, item_dict):
+    # List comprehension (Fancy dictionary shortcut)
+    item_data = ', '.join([f"'{key}': '{value}'" for key, value in item_dict.items()])
     query = f"""
-        INSERT {{ 'item_name': '{item_name}', 'item_description': '{item_description}' }} INTO items
+        INSERT {{ {item_data} }} INTO {collection}
         RETURN NEW
     """
     return execute_query(query)
-############################################################
-def get_items():
+# READ ######################################################
+def get_items(collection):
     query = f"""
-            FOR item IN items 
-            SORT item._key DESC
-            RETURN item
+            FOR doc IN {collection} 
+            SORT doc._key DESC
+            RETURN doc
         """
     return execute_query(query)
-############################################################
-def update_item(key, attribute, new_value):
+# UPDATE ####################################################
+def update_item(collection, key, new_value):
     query = f"""
-        FOR item IN items FILTER item._key == '{key}'
-        UPDATE item WITH {{ {attribute}: '{new_value}' }} IN items
+        FOR doc IN {collection} FILTER doc._key == '{key}'
+        UPDATE doc WITH {new_value}  IN {collection}
         RETURN NEW
     """
     return execute_query(query)
-############################################################
-def delete_item(key):
+# DELETE ####################################################
+def delete_item(collection, key):
     query = f"""
-        FOR item in items FILTER item._key == '{key}' 
-        REMOVE item IN items
+        FOR doc in {collection} FILTER doc._key == '{key}' 
+        REMOVE doc IN {collection}
         RETURN OLD
     """
     return execute_query(query)
 ############################################################
 # ROUTES ###################################################
-@get("/arango/items")
-def _():
+@get("/arango/<collection>")
+def _(collection):
     try:
-        return get_items()
+        return get_items(collection)
     except Exception as ex:
         ic(ex)
         return ex
     finally:
         pass
 
-@post("/arango/items")
-def _():
+@post("/arango/create_collection/<collection_name>")
+def create_collection(collection_name):
+    collection_url = url + 'collection'
+    body = { "name" : collection_name }
+    response = requests.post(collection_url, json = body)
+
+    if response.status_code == 200:
+        return f"Collection with name: {collection_name} has been created!"
+    else:
+        raise Exception(f'Request failed with status code: {response.status_code}')
+
+@post("/arango/<collection>")
+def _(collection):
     try:
-        item_name = request.forms.get("frm_item_name")
-        item_description = request.forms.get("frm_item_description")
-        
-        return create_item(item_name, item_description)
+        document_body = dict(request.forms)
+        return create_item(collection, document_body)
     except Exception as ex:
         ic(ex)
         return ex
     finally:
         pass
 
-@put('/arango/items/item_name/<key>')
-def _(key):
+@put('/arango/<collection>/<key>')
+def _(collection, key):
     try:
-        new_item_name = request.forms.get(f"{key}_frm_item_name")
-
-        return update_item(key, 'item_name', new_item_name)
+        document_body = dict(request.forms)
+        return update_item(collection, key, document_body)
     except Exception as ex:
         ic(ex)
         return ex
     finally:
         pass
 
-@put('/arango/items/item_description/<key>')
-def _(key):
-    try:
-        new_item_description = request.forms.get(f"{key}_frm_item_description")
+@delete("/arango/delete_collection/<collection_name>")
+def delete_collection(collection_name):
+    collection_url = url + f'collection/{collection_name}'
+    ic(collection_url)
+    response = requests.delete(collection_url)
 
-        return update_item(key, 'item_description', new_item_description)
-    except Exception as ex:
-        ic(ex)
-        return ex
-    finally:
-        pass
+    if response.status_code == 200:
+        return f"Collection with name: {collection_name} has been deleted!"
+    else:
+        raise Exception(f'Request failed with status code: {response.status_code}')
 
-@delete("/arango/items/<key>")
-def _(key):
+@delete("/arango/<collection>/<key>")
+def _(collection, key):
     try:
-        return delete_item(key)
+        return delete_item(collection, key)
     except Exception as ex:
         ic(ex)
         return ex
